dd62b8138f228ba452a81f8135406f0d9508fcba
ModischFabrications <modisch.fabrications@gmail.com>
2020-04-01 22:58:36 +0200

    use bootstrap and rename testjob keys to more useful things

--- src/components/SolverInput.vue
+++ src/components/SolverInput.vue
@@ -1,26 +1,40 @@
 <template>
-    <div id="solver-input">
-        <span class="border border-dark"></span>
-        <p>This will eventually be an input field</p>
-        <table>
-            <tr>
-                <th>A1</th>
-                <th>A2</th>
-            </tr>
-            <tr>
-                <th>B1</th>
-                <th>B2</th>
-            </tr>
-        </table>
-        <p> max size = 1500</p>
-        <p>cut width = 2</p>
-        <p> quantity size img_size</p>
-        <!-- pull to reorder -->
-        <p> # 350 4 [=======|.......]</p>
-        <p> # 150 1 [==|............]</p>
-        <p> # 500 12 [============|..] </p>
-        <p>[Add a new size]</p>
-        <!-- no idea how to visualize that -->
+    <div id="solver-input" class="solver_input">
+        <h2>Input</h2>
+
+        <b-row>
+            <b-col sm="2">
+                <label for="input_max_length">Maximum Length:</label>
+            </b-col>
+            <b-col sm="2">
+                <!-- add validation and set state accordingly -->
+                <b-form-input id="input_max_length" type="number" :state="false" v-model="job.max_length" min="1"
+                              max="9999" placeholder="Enter maximum length"></b-form-input>
+            </b-col>
+        </b-row>
+
+        <b-row>
+            <b-col sm="2">
+                <label for="input_cut_width">Cut width:</label>
+            </b-col>
+            <b-col sm="2">
+                <!-- relative validation? -->
+                <b-form-input id="input_cut_width" type="number" :state="false" v-model="job.cut_width" min="0"
+                              max="input_max_length.value/10"
+                              placeholder="Enter cutting width"></b-form-input>
+            </b-col>
+        </b-row>
+
+        <b-table sticky-header=true hover outlined :items="job.target_sizes" :fields="['quantity', 'length']">
+            <!--            <template v-slot:cell(amount)="amount">
+                            <b-form-input type="number" v-model="amount"></b-form-input>
+                        </template>-->
+            <!-- add interactive third row with size -->
+            <!-- [=======|.......] -->
+            <!-- make table modifiable -->
+            <!-- pull to reorder -->
+            <!-- [Add a new size] -->
+        </b-table>
     </div>
 </template>
 
@@ -40,6 +54,11 @@
     }
 </script>
 
+<!-- no idea why borders are broken -->
 <style scoped>
-
+    .solver_input {
+        border: blueviolet;
+        border-radius: 8px;
+        padding: 16px;
+    }
 </style>
