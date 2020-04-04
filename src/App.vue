e9c491ae7aed8866bcf7e88723377fb040af86da
ModischFabrications <modisch.fabrications@gmail.com>
2020-04-04 21:56:21 +0200

    show about page

--- src/App.vue
+++ src/App.vue
@@ -9,22 +9,15 @@
             </b-navbar-brand>
 
             <b-navbar-nav class="ml-auto">
-
-                <b-nav-item href="#">About</b-nav-item>
-                <!-- explain why/how I did it -->
-                <!-- fold out/popup short explanation -->
-                <!-- donate -->
-                <!-- github link -->
-
-                <!-- make icon: fontawesome? -->
-                <!--                <b-nav-item href="https://github.com/ModischFabrications/CutSolverFrontend">GitHub</b-nav-item>-->
+                <b-nav-item @click="$bvModal.show('about-modal')">About</b-nav-item>
             </b-navbar-nav>
         </b-navbar>
+
+        <About></About>
 
         <!-- side by side on desktop, top to bottom on mobile -->
         <SolverInput></SolverInput>
 
-        <!-- Button "solve now" that collects input, calls API and displays output -->
         <!-- centered and square on desktop, long bar button on mobile -->
         <b-button pill size="lg" type='submit' @click="startSolving">Solve</b-button>
 
@@ -42,6 +35,7 @@
     import {Result} from "@/components/data/Result";
     // example values
     import json_testresult from "./tests/data/testresult.json"
+    import About from "@/components/About";
     // stupid, better way?
     let testresult = new Result(json_testresult.solver_type, json_testresult.time_ms, json_testresult.lengths);
 
@@ -50,6 +44,7 @@
     export default {
         name: 'App',
         components: {
+            About,
             SolverOutput,
             SolverInput,
             // ...
@@ -69,8 +64,9 @@
                 console.log(SolverInput.computed.job());
 
                 // TODO: call API, -1 is an invalid value
-                let reply = (Math.random() > 0.2) ? testresult : -1;
+                let reply = (Math.random() > 0.2) ? testresult : new Result();
                 console.assert(reply !== null);
+                console.log(reply);
 
                 this.result = reply;
                 this.showResult = true;
---
+++ src/components/About.vue
@@ -0,0 +1,36 @@
+<template>
+    <b-modal id="about-modal" centered scrollable title="About" header-bg-variant="light" hide-footer>
+        <!-- explain how it calculates -->
+
+        <p>This project was born out of the need to cut lengths of steel tubing. Ordering everything
+            pre-cut is easy, but costs at least 30% more. I had everything I needed at home, but I needed to know how
+            many stock bars to order and where to cut them. I finished that project years ago, but the problem keeps
+            arising, which is why I finally caved in and developed this project. </p>
+
+        <p>Look at more things I do: </p>
+        <!-- IG, Thingiverse, Website, XING?, ... -->
+
+        <p>I will be forever grateful if you donate something to keep this website up, right now I am
+            paying around 5€/month to host this service. </p>
+        <!-- donate -->
+
+        <!-- make icon: fontawesome? -->
+        <!-- github link -->
+        <!-- <b-nav-item href="https://github.com/ModischFabrications/CutSolverFrontend">GitHub</b-nav-item>-->
+        <p>Check out the code if you want to see how I made it, you are welcome to add to it if you see any
+            improvements :) </p>
+
+        <p>You can send jobs to the backend yourself, see ## </p>
+    </b-modal>
+</template>
+
+<script>
+    export default {
+        name: "About",
+
+    }
+</script>
+
+<style scoped>
+
+</style>
