3d6a6279af658c054d38fa1a8bb185d586d1c0b9
ModischFabrications <modisch.fabrications@gmail.com>
2020-04-04 22:37:31 +0200

    extract valid method to data class

--- src/components/SolverOutput.vue
+++ src/components/SolverOutput.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- 1. hidden, 2. valid: show, 3. invalid: warning -->
-    <div v-if="valid(result)" id="solver-output" class="solver_output">
+    <div v-if="result.valid()" id="solver-output" class="solver_output">
         <h2>Output</h2>
         <b-table sticky-header=true hover outlined :items="result.lengths">
         </b-table>
@@ -21,15 +21,6 @@
                 required: true
             }
         },
-        methods: {
-            valid(result) {
-                return (result !== null &&
-                    result.solver_type !== undefined && result.solver_type.length > 0 &&
-                    result.time_ms !== undefined && result.time_ms > 0 &&
-                    result.lengths !== undefined && result.lengths.length > 0
-                );
-            }
-        }
     }
 </script>
 
--- src/components/data/Result.js
+++ src/components/data/Result.js
@@ -1,5 +1,14 @@
-export function Result(solver_type, time_ms, lengths) {
-    this.solver_type = solver_type;
-    this.time_ms = time_ms;
-    this.lengths = lengths;
+export class Result {
+    constructor(solver_type, time_ms, lengths) {
+        this.solver_type = solver_type;
+        this.time_ms = time_ms;
+        this.lengths = lengths;
+    }
+
+    valid() {
+        return (this.solver_type !== undefined && this.solver_type.length > 0 &&
+            this.time_ms !== undefined && this.time_ms > 0 &&
+            this.lengths !== undefined && this.lengths.length > 0
+        );
+    }
 }
