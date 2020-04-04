a261c829d20c0047d339238baec5074ab132a471
ModischFabrications <modisch.fabrications@gmail.com>
2020-04-04 23:07:33 +0200

    add about text, close #4

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,17 @@
 <template>
     <b-modal id="about-modal" centered scrollable title="About" header-bg-variant="light" hide-footer>
-        <!-- explain how it calculates -->
+        <!-- might need to be split into different parts or collapses instead... -->
+        <h5>What it is</h5>
+        <p>This service collects your input, sends it to a big calculator of mine, allows it to calculate for a while
+            and displays the received result to you.
+        </p>
+        <p><i>Could this be done on this website, on my device, in the background?</i> Probably,
+            that might even be faster, but the calculator was here first ¯\_(ツ)_/¯ </p>
+        <p><i>Do you steal my valuable data and sell it to illegal drug dealers on the black market?</i> No, is that
+            something that people do? Do I need to worry about that? To be honest, I wouldn't know what to do with it,
+            if you have a nice welding project send me an email instead so I can look at your fine work.</p>
 
+        <h5>Why it exists</h5>
         <p>This project was born out of the need to cut lengths of steel tubing. Ordering everything
             pre-cut is easy, but costs at least 30% more. I had everything I needed at home, but I needed to know how
             many stock bars to order and where to cut them. I finished that project years ago, but the problem keeps
@@ -20,7 +30,8 @@
         <p>Check out the code if you want to see how I made it, you are welcome to add to it if you see any
             improvements :) </p>
 
-        <p>You can send jobs to my backend all by yourself, see ## </p>
+        <p>You can send also jobs to my backend all by yourself, see ## . Please don't abuse or break it. Feel free to
+            send an issue or merge request or something if you find a vulnerability.</p>
     </b-modal>
 </template>
 
