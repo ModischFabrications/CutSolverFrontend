4ce779dc23ccedab4aa60086aca988a172459ed3
ModischFabrications <modisch.fabrications@gmail.com>
2020-04-04 22:53:33 +0200

    add outdated browser test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import VueBrowserUpdate from '@sum.cumo/vue-browserupdate';
 
 Vue.config.productionTip = false;
 
@@ -11,6 +14,14 @@ Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
 
+Vue.use(VueBrowserUpdate, {
+    options: {
+        // I'm doing my part for a clean and friendly environment without toxic waste everywhere
+        text: "Your browser ({brow_name}) is outdated and old enough to make me cry about it on the internet, <a{up_but}>update</a> it please!"
+    },
+    test: false,
+});
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app');
