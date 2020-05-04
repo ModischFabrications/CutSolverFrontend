b87db0a7eb246ab6c93189b02bfa3c295ab752bf
ModischFabrications <modisch.fabrications@gmail.com>
2020-05-04 21:40:33 +0200

    fix jest tests

--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -1,17 +1,26 @@
-import { shallowMount } from '@vue/test-utils'
+import {shallowMount, createLocalVue, mount} from '@vue/test-utils'
 import App from "@/App";
 
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+
+// create an extended `Vue` constructor to install plugins locally
+const localVue = createLocalVue()
+localVue.use(BootstrapVue)
+localVue.use(IconsPlugin);
+
+
 describe('App.vue', () => {
-  it('is visible', () => {
-    const wrapper = shallowMount(App)
-
-    expect(wrapper.isVisible()).toBe(true)
-  })
-
-  it('is Vue Instance', () => {
-    const wrapper = shallowMount(App)
+  it('is proper instance', () => {
+    // pass `localVue` to the mount options to actually use it
+    const wrapper = shallowMount(App, {localVue})
 
     expect(wrapper.isVueInstance()).toBe(true)
+  })
+
+  it('is visible', () => {
+    const wrapper = shallowMount(App, {localVue})
+
+    expect(wrapper.isVisible()).toBe(true)
   })
 
   // it('renders navbar', () => {
